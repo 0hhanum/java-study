1ff6f6ccbd89b0bf821596a1f1b8c66ca1251f8d
hanum <rntls123@naver.com>
2021-09-02 01:52:52 +0900

    09.02 Variable, public, static, void

---
+++ src/hello.java
@@ -0,0 +1,13 @@
+//public class hello {
+//    public static void main(String[] args){
+//        System.out.println("Hello");
+//    }
+//}
+
+public class hello {
+    public static void main(String[] args){
+        int x, y;
+        x = 12; y = 3;
+        System.out.println(y);
+    }
+}
