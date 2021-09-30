c7ce3e17044017bb2e4ca63d11ed9bd912c4a4a8
hanum <rntls123@naver.com>
2021-09-30 23:54:50 +0900

    09.30 Assignment 2-1 annotation

--- src/Assignment_Hanoi.java
+++ src/Assignment_Hanoi.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Assignment_Hanoi {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("원반의 개수를 입력하세요.");
+        System.out.println("원반의 개수를 입력하세요. >");
         int plates = scanner.nextInt();
+        System.out.println("무슨 기둥으로 옮길 것인지 입력하세요. (b or c) > ");
+        char dest = scanner.next().charAt(0);
         scanner.close();
-        playHanoi(plates, 'a', 'c');
-//        playHanoi(plates, 'a', 'b');
-
+        playHanoi(plates, 'a', dest);
         System.out.println("" + countMoves(plates) + "회 이동하였습니다.");
     }
 
@@ -22,6 +22,21 @@ public class Assignment_Hanoi {
     }
 
     static void playHanoi(int plates, char from, char to){
+        /*
+        n 개의 원판을 다른 기둥으로 옮기는 하노이탑은 n-1 개의 원판을 다른 기둥에 임시적으로 옮기고
+        n 번째 원판을 목적 기둥으로 옮긴 뒤, 임시적으로 옮겨놨던 n-1 개의 원판을 다시 그 위로 옮기는 방식으로 수행됩니다.
+        즉, f(n) = 2 * f(n-1) + 1 로 표현할 수 있습니다.
+
+        단순히 이동 횟수를 구하는 것이 아니라, 이동 과정을 출력해야 했기에 "어디서 출발해 어디로 가냐" 에 초점을 맞추어 설계했습니다.
+
+        결국 하노이탑의 모든 과정이 n-1 개의 원판을 임시적으로 a -> (b or c) 로 옮긴 후 다시 (b or c) -> a 로 옮기는 과정이기에
+        from 과 to 를 인자로 주어 상황에 맞게 재귀호출 했으며, 이를 삼항연산자를 이용해 가위바위보 식으로 구현했습니다.
+
+        ex)
+        b 로 옮긴다 => a 에서 출발했는가? => c 로 n-1 개를 이동. => b 로 n 번째 원판 이동 => n-1 개를 c 에서 b 로 이동.
+                  => c 에서 출발했는가? => a 로 n-1 개를 이동. => b 로 n 번째 원판 이동 => n-1 개를 a 에서 b 로 이동.
+
+         */
         if (plates == 1) {
             printMove(1, from, to);
         } else {
