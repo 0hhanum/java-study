8dedb99d814d87bcaf202b3812df5229b829cff7
hanum <rntls123@naver.com>
2021-11-17 02:17:15 +0900

    11.17 Assignment 5_1 #try 2 Maybe success?

--- src/Assignment/Assignment5_1.java
+++ src/Assignment/Assignment5_1.java
@@ -7,7 +7,9 @@ import java.util.EmptyStackException;
 public class Assignment5_1 {
     public static void main(String[] args){
         // scanner 로 배열 길이, 각 막대의 높이 받아서 채우기.
-        int[] histogram = new int[8];
+//        int[] histogram = new int[8];
+        int[] histogram = {3,2,3,4,2,3,4,1};
+
         Stack stack = new Stack();
 
         int maxArea = 0;
@@ -23,21 +25,27 @@ public class Assignment5_1 {
                 // 스택이 비어있다면 현재 height 를 stack 에 push 후 다음 반복.
             }
             int width = 1;
+            System.out.println("Now Height = " + height);
+            System.out.println(topOfStack >= height);
             while (topOfStack >= height){ // topOfStack > height 라면 while 문을 돈다.
                 int topHeight = stack.pop();
                 int currentArea = topHeight * width;
+                System.out.println(currentArea);
+                System.out.println(width);
                 if(currentArea > maxArea) maxArea = currentArea; // pop() 으로 얻은 높이의 직사각형과 maxArea 를 비교.
                 width++; // 너비는 1 증가
                 try{
                     topOfStack = stack.peek();
-                } catch (EmptyStackException exception){
+                } catch (EmptyStackException exception){ // peek 할 원소가 없는 경우.
+                    currentArea = topHeight * (i + 1);
+                    if(currentArea > maxArea) maxArea = currentArea;
                     break; // stack 에 원소가 없으면 반복문 탈출.
                 }
             }
-            if (topOfStack < height) {
-                // topOfStack 이 height 보다 작다면 height 을 stack 에 push.
-                stack.push(height);
-            }
+//            if (topOfStack < height) {
+//                // topOfStack 이 height 보다 작다면 height 을 stack 에 push.
+            stack.push(height);
+//            }
         }
         System.out.println(maxArea);
 
